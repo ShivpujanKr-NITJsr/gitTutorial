b6093f0cdabf971444f4163516b36039acde5975
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-07-13 17:19:29 +0530

    this is new first commit for the project for dom

--- dom.js
+++ dom.js
@@ -70,14 +70,93 @@
 // id.createElement('li');
 
 
-var item=document.querySelector('.list-group-item:nth-child(2)');
-item.style.backgroundcolor='green';
-var item=document.querySelector('.list-group-item:nth-child(3)');
+// var item=document.querySelector('.list-group-item:nth-child(2)');
+// item.style.backgroundcolor='green';
+// var item=document.querySelector('.list-group-item:nth-child(3)');
 
-item.style.visibility='hidden';
+// item.style.visibility='hidden';
 
-var it=document.querySelectorAll('.list-group-item:nth-child(2)');
-it.style.backgroundcolor='green';
+// var it=document.querySelectorAll('.list-group-item:nth-child(2)');
+// it.style.backgroundcolor='green';
 
-var irt=document.querySelectorAll('.list-group-item:nth-child(odd)');
-irt.style.backgroundcolor='green';
+// var irt=document.querySelectorAll('.list-group-item:nth-child(odd)');
+// irt.style.backgroundcolor='green';
+
+// ******************************traversing dom//
+
+// var itemList=document.querySelector('#items');
+// parentnode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentelement
+// console.log(itemList.parentElement);
+// itemList.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//childnodes
+// console.log(itemList.childNodes);
+// var itemList=document.querySelector('#items');
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor='yellow';
+
+// //FirstCHild
+
+// console.log(itemList.firstChild);
+// // first elementchild
+// var itemList=document.querySelector('#items');
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello 1';
+
+//lastchild
+// var itemList=document.querySelector('#items');
+// console.log(itemList.lastChild);
+//lastelementchild
+// var itemList=document.querySelector('#items');
+
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='Hello 4';
+
+// var itemList=document.querySelector('#items');
+//nextsibling
+// console.log(itemList.nextSibling);
+// nextElementSibling
+// var itemList=document.querySelector('#items');
+// console.log(itemList.previousSibling);
+// //previoussielementsibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color='green';
+
+// var itemList=document.querySelector('#items');
+
+// //createElement
+
+// // create a div
+
+// var newDiv= document.createElement('div');
+// // add class
+// newDiv.className='hello';
+// //add id
+// newDiv.id='hello1';
+// //add attr
+// newDiv.setAttribute('title','Hello Div');
+
+// // create text node
+// var newDivText=document.createTextNode('Hello world');
+// //add text to div
+// newDiv.appendChild(newDivText);
+
+// var container =document.querySelector('header .container');
+// var h1=document.querySelector('header h1');
+
+// console.log(newDiv);
+
+// // newDiv.style.fontSize='30px';
+// container.insertBefore(newDiv,h1);
+
+var header = document.getElementById('header-title');
+header.innerText = 'Hello ' + header.innerText;
+var listItem = document.querySelector('#items li');
+listItem.innerText = 'Hello ' + listItem.innerText;
